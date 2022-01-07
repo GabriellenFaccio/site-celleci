69f1ef1ab54285bf29ce4f12d550db14184da13d
Gabriellen Faccio <gabriellen.faccio@gmail.com>
2022-01-06 23:24:58 -0300

    Iniciando comandos testes

---
+++ src/components/Post.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Comment from './Comment';
+
+export default class Post extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            comments: [
+                { text: 'Bom Post!'}
+            ]
+        };
+    }
+
+    render (){
+        return (
+            <div>
+                <h2>{ this.props.title }</h2>
+                <form >
+                    <input />
+                    <button type="submit">Comentar</button>
+                </form>
+                { this.state.comments.map((comment, index) => {
+                    return <Comment key={index} text={comment.text} />
+                }) }
+            </div>
+            
+        );
+    }
+}
